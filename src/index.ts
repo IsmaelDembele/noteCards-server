1247cd156f2cb69e06862b96c839a55edc77029b
ismaelDembele <dembele.ismael@gmail.com>
2022-04-27 11:53:35 -0400

    deployment changes

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,21 @@ import authRoute from "./routes/authRoutes";
 import route from "./routes/routes";
 import mongoose from "mongoose";
 
+import "dotenv/config";
+const app = express();
+
 const main = async () => {
   // await mongoose.connect("mongodb://localhost:27017/notecard");
   await mongoose.connect(process.env.MONGO_URI as string);
+  console.log(app.get("env"));
 
   const port = process.env.PORT || 5000;
 
   const corsOptions = {
     origin: [process.env.ORIGIN as string],
     method: ["GET", "POST"],
-    credentials: false,
+    credentials: app.get("env") !== "development",
   };
-
-  const app = express();
 
   app.use(cors(corsOptions));
   app.use(express.json());
