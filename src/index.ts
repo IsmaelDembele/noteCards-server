af011c2224fd50a4e4731a562da68ba814ad11e7
ismaelDembele <dembele.ismael@gmail.com>
2022-04-27 10:18:21 -0400

    env variable added

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,13 @@ import route from "./routes/routes";
 import mongoose from "mongoose";
 
 const main = async () => {
-  await mongoose.connect("mongodb://localhost:27017/notecard");
+  // await mongoose.connect("mongodb://localhost:27017/notecard");
+  await mongoose.connect(process.env.MONGO_URI as string);
 
   const port = process.env.PORT || 5000;
 
   const corsOptions = {
-    origin: ["http://localhost:3000"],
+    origin: [process.env.ORIGIN as string],
     method: ["GET", "POST"],
     credentials: false,
   };
